ccd5839afbc2ef001e51b836bd14f03ea9fbc009
Trevor Manz <trevor.j.manz@gmail.com>
2020-09-17 00:47:36 -0400

    working prototype

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use std::collections::HashMap;
+use std::io::prelude::*;
 use std::{env, fs, io};
 
-#[derive(Debug)]
-struct Interval(u32, u32, u32, u32);
+#[derive(Debug, Clone, Copy)]
+struct Interval {
+    st: u32,
+    en: u32,
+    max: u32,
+    data: u32,
+}
 
 #[derive(Debug)]
 struct IITree {
@@ -14,69 +20,91 @@ impl IITree {
         IITree { a: Vec::new() }
     }
 
-    fn add(&mut self, start: u32, end: u32, max: u32) {
-        self.a.push(Interval(start, end, max, 0))
+    fn add(&mut self, st: u32, en: u32, max: u32, data: u32) {
+        self.a.push(Interval { st, en, max, data })
     }
 
-    fn index(&mut self) {
-        self.a.sort_unstable_by_key(|k| k.0);
+    fn index(&mut self) -> usize {
+        self.a.sort_unstable_by_key(|k| k.st);
+
         let (mut last, mut last_i, mut i) = (0, 1, 0);
         while i < self.a.len() {
-            last = self.a[i].1;
-            self.a[i].2 = last;
+            last = self.a[i].en;
+            self.a[i].max = self.a[i].en;
             last_i = i;
             i += 2;
         }
+
         let mut k = 1;
         while 1 << k <= self.a.len() {
-            let (mut i0, step) = (1 << k, 1 << (k + 1));
-            while i0 <= self.a.len() {
+            let step = 1 << (k + 1);
+            let mut i = (1 << k) - 1;
+            while i < self.a.len() {
                 let x = 1 << (k - 1);
-                let mut max_end = std::cmp::max(self.a[i].1, self.a[i - x].2);
-                let e = if i + x <= self.a.len() {
-                    self.a[i + x].1
+                let max = std::cmp::max(self.a[i].en, self.a[i - x].max);
+                let e = if i + x < self.a.len() {
+                    self.a[i + x].max
                 } else {
                     last
                 };
-                max_end = std::cmp::max(max_end, e);
-                self.a[i] = Interval(self.a[i].3, self.a[i].0, self.a[i].1, max_end);
-                i0 += step;
+                let max = std::cmp::max(e, max);
+                self.a[i].max = max;
+
+                i += step;
             }
+
             last_i = if (last_i >> k & 1) != 0 {
-                last_i + (1 << (k - 1))
-            } else {
                 last_i - (1 << (k - 1))
+            } else {
+                last_i + (1 << (k - 1))
             };
 
-            if last_i <= self.a.len() {
-                last = std::cmp::max(last, self.a[last_i].2);
+            if last_i < self.a.len() {
+                last = std::cmp::max(last, self.a[last_i].max);
             }
+
             k += 1;
         }
-    }
-}
 
-#[derive(Debug)]
-struct BedMap<'a> {
-    map: HashMap<&'a str, IITree>,
-}
-
-impl<'a> BedMap<'a> {
-    fn from_str(content: &'a str) -> Self {
-        let mut map: HashMap<&str, IITree> = HashMap::with_capacity(24);
-        for line in content.lines() {
-            let (chrom, start, end) = parse_line(line).expect("Failed to parse BED row.");
-            let tree = map.entry(chrom).or_insert(IITree::new());
-            tree.add(start, end, end);
-        }
-        Self { map }
+        k - 1
     }
 
-    fn index_trees(&mut self) {
-        // sort each grouping
-        for (_, tree) in self.map.iter_mut() {
-            tree.index();
+    fn overlap(&self, st: u32, en: u32) -> Vec<Interval> {
+        let mut h = 0;
+        while 1 << h <= self.a.len() {
+            h += 1;
         }
+        h -= 1;
+        let mut b: Vec<Interval> = Vec::new();
+        let mut stack: Vec<(usize, usize, usize)> = Vec::with_capacity(64);
+        stack.push(((1 << h) - 1, h, 0));
+        while let Some((x, h, w)) = stack.pop() {
+            if h <= 3 {
+                let mut i = x >> h << h;
+                let mut i1 = i + (1 << (h + 1)) - 1;
+                if i1 >= self.a.len() {
+                    i1 = self.a.len();
+                }
+                while i < i1 && self.a[i].st < en {
+                    if st < self.a[i].en {
+                        b.push(self.a[i]);
+                    }
+                    i += 1;
+                }
+            } else if w == 0 {
+                stack.push((x, h, 1));
+                let y = x - (1 << (h - 1));
+                if y >= self.a.len() || self.a[y].max > st {
+                    stack.push((y, h - 1, 0));
+                }
+            } else if x < self.a.len() && self.a[x].st < en {
+                if st < self.a[x].en {
+                    b.push(self.a[x]);
+                }
+                stack.push((x + (1 << (h - 1)), h - 1, 0));
+            }
+        }
+        b
     }
 }
 
@@ -89,34 +117,59 @@ fn parse_line(line: &str) -> Option<(&str, u32, u32)> {
 }
 
 fn main() -> io::Result<()> {
-    let mut args = env::args();
+    let path = env::args().nth(1).expect("Missing input file.");
+    let content = fs::read_to_string(path)?;
 
-    let path = args.nth(1).expect("Missing input file.");
-    let contents = fs::read_to_string(path)?;
-    let mut tree = BedMap::from_str(&contents);
+    let mut map: HashMap<&str, IITree> = HashMap::with_capacity(24);
+    for (i, line) in content.lines().enumerate() {
+        let (chrom, start, end) = parse_line(line).expect("Failed to parse BED row.");
+        let tree = map.entry(chrom).or_insert(IITree::new());
+        tree.add(start, end, end, i as u32);
+    }
 
-    tree.index_trees();
-    println!("{:#?}", tree);
+    for (_, tree) in map.iter_mut() {
+        tree.index();
+    }
 
-    // let path = args.nth(2).expect("Missing other file.");
-    // println!("{}", path);
+    let path = env::args().nth(2).expect("Missing file 2.");
+    let f = fs::File::open(path)?;
+    let mut reader = io::BufReader::new(f);
+    let mut line = String::new();
+
+    reader
+        .read_line(&mut line)
+        .expect("Failed to parse record.");
+    while !line.is_empty() {
+        let (chrom, st0, en0) = parse_line(&line).expect("failed to parse row.");
+        let (start, end, len, cov) = match map.get(chrom) {
+            Some(tree) => {
+                let a = tree.overlap(st0, en0);
+                let mut cov_st = 0;
+                let mut cov_en = 0;
+                let mut cov = 0;
+
+                for m in &a {
+                    let st1 = std::cmp::max(st0, m.st);
+                    let en1 = std::cmp::min(en0, m.en);
+                    if st1 > cov_en {
+                        cov += cov_en - cov_st;
+                        cov_st = st1;
+                        cov_en = en1;
+                    } else {
+                        cov_en = std::cmp::max(cov_en, en1);
+                    }
+                }
+                cov += cov_en - cov_st;
+                (st0, en0, a.len(), cov)
+            }
+            None => (st0, en0, 0, 0),
+        };
+        println!("{}\t{}\t{}\t{}\t{}", chrom, start, end, len, cov);
+        line.clear();
+        reader
+            .read_line(&mut line)
+            .expect("Failed to parse record.");
+    }
 
     Ok(())
 }
-
-// fn read_bed_by_line() -> io::Result<()> {
-//     let path = std::env::args().nth(1).unwrap();
-//
-//     let f = fs::File::open(path)?;
-//     let mut reader = io::BufReader::new(f);
-//
-//     let mut line = String::new();
-//     while let Ok(_) = reader.read_line(&mut line) {
-//         let (chrom, start, end) = parse_line(&line).expect("Failed to parse row.");
-//         // do something with line
-//
-//         line.clear();
-//     }
-//
-//     Ok(())
-// }
